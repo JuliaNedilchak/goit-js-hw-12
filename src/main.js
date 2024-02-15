e221566c9655166721910ae0754663da970e8fde
JuliaNedilchak <julianedilchak@gmail.com>
2024-02-15 14:45:46 +0000

    part 1

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import iziToast from 'izitoast';
 import SimpleLightbox from 'simplelightbox';
+import axios from 'axios';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import 'izitoast/dist/css/iziToast.min.css';
 
@@ -16,29 +17,29 @@ function onFormSubmit(e) {
   form.reset();
 }
 
-function getImages(imageName) {
+async function getImages(imageName) {
   const BASE_URL = 'https://pixabay.com/api/';
   const params = `?key=42244412-4baf4dd8f3efd9c6d484c6d30&q=${imageName}&image_type=photo&orientation=horizontal&safesearch=true`;
 
   const url = BASE_URL + params;
   loader.style.display = 'block';
-  return fetch(url)
-    .then(res => res.json())
+  const res = await axios.get(url);
 
-    .then(images => {
-      if (images.hits.length === 0) {
-        gallery.innerHTML = '';
-        loader.style.display = 'none';
+  try {
+    const images = await res;
+    if (images.hits.length === 0) {
+      gallery.innerHTML = '';
+      loader.style.display = 'none';
 
-        iziToast.error({
-          message:
-            'Sorry, there are no images matching your search query. Please try again!',
-        });
-      } else {
-        loader.style.display = 'none';
-        const markup = images.hits
-          .map(image => {
-            return `<li class="item"> <a class="gallery-link" href="${image.largeImageURL}">
+      iziToast.error({
+        message:
+          'Sorry, there are no images matching your search query. Please try again!',
+      });
+    } else {
+      loader.style.display = 'none';
+      const markup = images.hits
+        .map(image => {
+          return `<li class="item"> <a class="gallery-link" href="${image.largeImageURL}">
     <img
       class="gallery-image"
      
@@ -53,19 +54,19 @@ function getImages(imageName) {
   <li class="group"><p class="desc">downloads<span class="amount"> ${image.downloads}</span></p></li>
   </ul>
   </li>`;
-          })
-          .join('');
+        })
+        .join('');
 
-        gallery.innerHTML = markup;
-      }
-      const lightbox = new SimpleLightbox('.gallery a', {
-        captionsData: 'alt',
-        captionDelay: 250,
-      });
-      lightbox.refresh();
-    })
-    .catch(error => {
-      toastError(`Error: ${error}`);
-      throw error;
+      gallery.innerHTML = markup;
+    }
+    const lightbox = new SimpleLightbox('.gallery a', {
+      captionsData: 'alt',
+      captionDelay: 250,
     });
+    lightbox.refresh();
+  } catch (error) {
+    toastError(`Error: ${error}`);
+    throw error;
+  }
+  return;
 }
